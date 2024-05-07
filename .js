a0e0d5acd9bf3c0aa9fc180d9f2abd82e1a38589
Tematzuka8 <hyper.creative.tematzuka8@gmail,com>
2024-05-07 15:33:06 +0900

    Add:js

--- .js
+++ .js
@@ -1,1 +1,9 @@
-a
+chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.tabs.query({}, function(tabs) {
+      if (tabs.length > 10) {
+        // 10個以上のタブが開かれている場合、最後のタブを閉じる
+        chrome.tabs.remove(tab.id);
+      }
+    });
+  });
+  
