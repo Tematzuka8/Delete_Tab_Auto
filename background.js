56c063cfc5e5d793d0ae593fa3b07c2b521e41e8
Tematzuka8 <hyper.creative.tematzuka8@gmail,com>
2024-05-16 14:13:44 +0900

    add background.js

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.tabs.query({}, function(tabs) {
+      if (tabs.length > 10) {
+        // 10個以上のタブが開かれている場合、最後のタブを閉じる
+        chrome.tabs.remove(tab.id);
+      }
+    });
+  });
+  
